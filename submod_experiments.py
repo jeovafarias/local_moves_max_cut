a5632ca3f82741aae8103f69a5d1a49a93e2b7ea
Alice Paul <ajp336@cornell.edu>
2018-08-16 14:22:20 -0400

    Implemented submodular alg and basic tests

---
+++ submod_experiments.py
@@ -0,0 +1,232 @@
+import os
+import sys
+import time
+from numpy.core.multiarray import ndarray
+import numpy as np
+import sklearn.metrics.pairwise as skl
+
+import clustering_utils as cu
+import data_generation_tools as dg
+import data_visualization_tools as dv
+import moves
+import submod_alg as sa
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+np.set_printoptions(linewidth=1000, precision=4, threshold=np.nan, suppress=True)
+
+
+# noinspection PyStringFormat
+def save_data(purities, energies, percentages_energy, times, dirpath, filename="test"):
+
+    f = open(str(dirpath) + '/' + filename + '.dat', 'a')
+    f.write('prop|value\n')
+    f.write('exp|%d\n' % num_trials)
+
+    f.write('tim|%.4f\n' % np.mean(times))
+    f.write('tim_std|%.4f\n' % np.std(times))
+
+    f.write('pur|%.4f\n' % np.mean(purities))
+    f.write('pur_std|%.4f\n' % np.std(purities))
+    f.write('pur_max|%.4f\n' % np.max(purities))
+
+    f.write('ene|%.4f\n' % np.mean(energies))
+    f.write('ene_std|%.4f\n' % np.std(energies))
+    f.write('ene_max|%.4f\n' % np.max(energies))
+
+    f.write('per|%.2f\n' % (100 * np.mean(percentages_energy)))
+    f.write('per_std|%.4f\n' % np.std(percentages_energy))
+    f.write('per_max|%.2f\n' % (100 * np.max(percentages_energy)))
+    f.close()
+
+
+# MAIN CODE ============================================================================================================
+def run_test(n, K, sigma_2, num_trials, random_init, dir_name='test'):
+    time_start = time.time()
+
+    # Data generation parameters ---------------------------------------------------------------------------------------
+    N = n * K
+    use_prev_p = 0
+    shuffle_data = 0
+    params_data = {'sigma_1': 1, 'sigma_2': sigma_2, 'K': K, 'dim_space': 2, 'pop_interv': [n, n],
+                   'use_prev_p': use_prev_p, 'shuffle': shuffle_data}
+
+    # Data generation and visualization --------------------------------------------------------------------------------
+    P, ground_truth = dg.generate_data_random(params_data)
+    C = skl.pairwise_distances(P, metric='sqeuclidean')
+    #dv.plot_data(P, K, ground_truth, 2)
+
+    # Save plot
+    experiment_id = 0
+    while os.path.exists(dir_name + '/' + str(experiment_id)):
+        experiment_id += 1
+    dirpath = dir_name + '/' + str(experiment_id)
+    os.makedirs(dirpath)
+    dv.plot_data(P, K, ground_truth, 2, show_legend=False, show_data=False, save_to_file=True,
+                 file_name=dirpath + '/id_' + str(experiment_id) + '_GT',
+                 title='Ground Truth (N = %d, K = %d)' % (n*K, K))
+
+    # Get other format
+    V = [i for i in range(N)]
+    E = []
+    w = []
+    for i in range(N):
+        for j in range(N):
+            if i != j:
+                E.append([i,j])
+                w.append(C[i,j])
+
+    # Other parameters -------------------------------------------------------------------------------------------------
+    use_IPM = 1
+    num_max_it = 20
+
+    ab_pur_t, ab_ene_t, ab_per_t, ab_tim_t = np.zeros(num_trials), np.zeros(num_trials), \
+                                             np.zeros(num_trials), np.zeros(num_trials)
+    ae_pur_t, ae_ene_t, ae_per_t, ae_tim_t = np.zeros(num_trials), np.zeros(num_trials), \
+                                             np.zeros(num_trials), np.zeros(num_trials)
+    ab_sub_pur_t, ab_sub_ene_t, ab_sub_per_t, ab_sub_tim_t = np.zeros(num_trials), np.zeros(num_trials), \
+                                                     np.zeros(num_trials), np.zeros(num_trials)
+    ae_sub_pur_t, ae_sub_ene_t, ae_sub_per_t, ae_sub_tim_t = np.zeros(num_trials), np.zeros(num_trials), \
+                                                     np.zeros(num_trials), np.zeros(num_trials)
+    ls_pur_t, ls_ene_t, ls_per_t, ls_tim_t = np.zeros(num_trials), np.zeros(num_trials), \
+                                             np.zeros(num_trials), np.zeros(num_trials)
+
+    # Iterate ----------------------------------------------------------------------------------------------------------
+    for t in range(num_trials):
+        print t
+        if random_init:
+            lb_init = np.random.randint(0, K, N)
+            ab_sequence = None
+        else:
+            lb_init = np.zeros(N, dtype=int)
+            ab_sequence = np.array([np.random.choice(K, K, replace=False), np.random.choice(K, K, replace=False)])
+
+        lb_dict = dict()
+        for i in range(N):
+            lb_dict[i] = lb_init[i]
+            
+        start_t = time.time()
+        lb_ab = moves.large_move_maxcut(C, K, lb_init,
+                                        move_type="ab", ab_sequence=ab_sequence,
+                                        num_max_it=num_max_it, use_IPM=use_IPM)
+        ab_pur_t[t], ab_ene_t[t], ab_per_t[t] = cu.stats_clustering(C, lb_ab, ground_truth)
+        ab_tim_t[t] = time.time() - start_t
+
+        start_t = time.time()
+        lb_ab_sub_dict = sa.localSearchSubmod(V,E,w,K,lb_dict,moveType="ab",maxIt=num_max_it)
+        lb_ab_sub = [lb_ab_sub_dict[i] for i in range(N)]
+        ab_sub_pur_t[t], ab_sub_ene_t[t], ab_sub_per_t[t] = cu.stats_clustering(C, lb_ab_sub, ground_truth)
+        ab_sub_tim_t[t] = time.time() - start_t
+
+        start_t = time.time()
+        lb_ae = moves.large_move_maxcut(C, K, lb_init,
+                                        move_type="ae", ab_sequence=ab_sequence,
+                                        num_max_it=num_max_it, use_IPM=use_IPM)
+        ae_pur_t[t], ae_ene_t[t], ae_per_t[t] = cu.stats_clustering(C, lb_ae, ground_truth)
+        ae_tim_t[t] = time.time() - start_t
+
+        start_t = time.time()
+        lb_ae_sub_dict = sa.localSearchSubmod(V,E,w,K,lb_dict,moveType="ab",maxIt=num_max_it)
+        lb_ae_sub = [lb_ae_sub_dict[i] for i in range(N)]
+        ae_sub_pur_t[t], ae_sub_ene_t[t], ae_sub_per_t[t] = cu.stats_clustering(C, lb_ae_sub, ground_truth)
+        ae_sub_tim_t[t] = time.time() - start_t
+
+        #start_t = time.time()
+        #lb_aebs = moves.large_move_maxcut(C, K, lb_init,
+        #                                  move_type="ae_bs", ab_sequence=ab_sequence,
+        #                                  num_max_it=num_max_it, use_IPM=use_IPM)
+        #aebs_pur_t[t], aebs_ene_t[t], aebs_per_t[t] = cu.stats_clustering(C, lb_aebs, ground_truth)
+        #aebs_tim_t[t] = time.time() - start_t
+
+        start_t = time.time()
+        lb_ls = cu.local_search(C, K, lb_init, num_max_it=num_max_it)
+        ls_pur_t[t], ls_ene_t[t], ls_per_t[t] = cu.stats_clustering(C, lb_ls, ground_truth)
+        ls_tim_t[t] = time.time() - start_t
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+    # Save data --------------------------------------------------------------------------------------------------------
+    #experiment_id = 0
+    #while os.path.exists(dir_name + '/' + str(experiment_id)):
+    #    experiment_id += 1
+    #dirpath = dir_name + '/' + str(experiment_id)
+    #os.makedirs(dirpath)
+
+    np.savetxt(str(dirpath) + '/P.dat', P)
+    np.savetxt(str(dirpath) + '/gt.dat', ground_truth, fmt='%d')
+    np.savetxt(str(dirpath) + '/lb_init.dat', lb_init)
+    np.savetxt(str(dirpath) + '/ab_sequence.dat', ab_sequence)
+
+    print("ALPHA-BETA SWAP (id %d):" % experiment_id)
+    print("> Mean time: %.4f + %.4f" % (np.mean(ab_tim_t), np.std(ab_tim_t)))
+    print("> Mean pur:  %.4f + %.4f" % (np.mean(ab_pur_t), np.std(ab_pur_t)))
+    save_data(ab_pur_t, ab_ene_t, ab_per_t, ab_tim_t, dirpath, filename="ab_results")
+
+    print("ALPHA EXPANSION (id %d):" % experiment_id)
+    print("> Mean time: %.4f + %.4f" % (np.mean(ae_tim_t), np.std(ae_tim_t)))
+    print("> Mean pur:  %.4f + %.4f" % (np.mean(ae_pur_t), np.std(ae_pur_t)))
+    save_data(ae_pur_t, ae_ene_t, ae_per_t, ae_tim_t, dirpath, filename="ae_results")
+
+    print("ALPHA-BETA SUBMOD (id %d):" % experiment_id)
+    print("> Mean time: %.4f + %.4f" % (np.mean(ab_sub_tim_t), np.std(ab_sub_tim_t)))
+    print("> Mean pur:  %.4f + %.4f" % (np.mean(ab_sub_pur_t), np.std(ab_sub_pur_t)))
+    save_data(ab_sub_pur_t, ab_sub_ene_t, ab_sub_per_t, ab_sub_tim_t, dirpath, filename="ab_sub_results")
+
+    print("ALPHA EXP SUBMOD (id %d):" % experiment_id)
+    print("> Mean time: %.4f + %.4f" % (np.mean(ae_sub_tim_t), np.std(ae_sub_tim_t)))
+    print("> Mean pur:  %.4f + %.4f" % (np.mean(ae_sub_pur_t), np.std(ae_sub_pur_t)))
+    save_data(ae_sub_pur_t, ae_sub_ene_t, ae_sub_per_t, ae_sub_tim_t, dirpath, filename="ae_sub_results")
+
+    print("LOCAL SEARCH (id %d):" % experiment_id)
+    print("> Mean time: %.4f + %.4f" % (np.mean(ls_tim_t), np.std(ls_tim_t)))
+    print("> Mean pur:  %.4f + %.4f" % (np.mean(ls_pur_t), np.std(ls_pur_t)))
+    save_data(ls_pur_t, ls_ene_t, ls_per_t, ls_tim_t, dirpath, filename="ls_results")
+
+    print("Total time (id %d): %.4f s\n" % (experiment_id, time.time() - time_start))
+
+
+if __name__ == "__main__":
+    dir_name = 'alice_experiments'
+    # dir_name = 'experiments_non_rand_int'
+    num_trials = 10
+    random_init = False
+
+    points_per_cluster = 10
+    K = 3
+    sigmas = [0.05]
+    for e in range(len(sigmas)):
+         print('EXP. %d - 1 =================================================================================' % (e + 1))
+         sigma_1 = 1
+         sigma_2 = sigmas[e]
+         run_test(points_per_cluster, K, sigma_2, num_trials, random_init, dir_name=dir_name)
+    print("SET FINISHED ========================================================================== \n")
+
+##    points_per_cluster = 8
+##    K = 4
+##    sigmas = [0.05, 0.1, 0.15, 0.2, 0.2]
+##    for e in range(len(sigmas)):
+##        print('EXP. %d - 2 =================================================================================' % (e + 1))
+##        sigma_1 = 1
+##        sigma_2 = sigmas[e]
+##        run_test(points_per_cluster, K, sigma_2, num_trials, random_init, dir_name=dir_name)
+##    print("SET FINISHED ========================================================================== \n")
+##
+##    points_per_cluster = 2
+##    K = 4
+##    sigmas = [0.15, 0.15]
+##    for e in range(len(sigmas)):
+##        print('EXP. %d - 3 =================================================================================' % (e + 1))
+##        sigma_1 = 1
+##        sigma_2 = sigmas[e]
+##        run_test(points_per_cluster, K, sigma_2, num_trials, random_init, dir_name=dir_name)
+##    print("SET FINISHED ========================================================================== \n")
+##
+##    points_per_cluster = 10
+##    K = 10
+##    sigmas = [0.1, 0.1, 0.1, 0.15, 0.15]
+##    for e in range(len(sigmas)):
+##        print('EXP. %d - 4 =================================================================================' % (e + 1))
+##        sigma_1 = 1
+##        sigma_2 = sigmas[e]
+##        run_test(points_per_cluster, K, sigma_2, num_trials, random_init, dir_name=dir_name)
+##    print("SET FINISHED ========================================================================== \n")
---
+++ test_submod_alg.py
@@ -0,0 +1,297 @@
+import submod_alg as sa
+import unittest
+
+places = 6
+
+#---------------------- General Utilities ------------------------#
+
+class TestEnergy(unittest.TestCase):
+    def test_empty(self):
+        V = [0,1,2]
+        E = []
+        w = []
+
+        labels = {1:0,2:0,0:0}
+        self.assertEqual(sa.energy(V,E,w,labels),0.)
+        
+    def test_edges(self):
+        V = [0,1,2]
+        E = [[1,2],[0,1],[2,0]]
+        w = [1.0, 2.0, 0.5]
+
+        labels = {1:0,2:0,0:0}
+        self.assertEqual(sa.energy(V,E,w,labels),0.)
+
+        labels[1] = 1
+        self.assertEqual(sa.energy(V,E,w,labels),3.)
+
+        labels[2] = 2
+        self.assertEqual(sa.energy(V,E,w,labels),3.5)
+        
+    def test_hyperedges(self):
+        V = [0,1,2,3]
+        E = [[0,1,2],[2,1,3]]
+        w = [1.0, 2.0]
+
+        labels = {1:0,2:0,0:0,3:0}
+        self.assertEqual(sa.energy(V,E,w,labels),0.)
+
+        labels[3] = 1
+        self.assertEqual(sa.energy(V,E,w,labels),2.)
+
+        labels[2] = 1
+        self.assertEqual(sa.energy(V,E,w,labels),3.)
+
+class TestSubmodEval(unittest.TestCase):
+    def test_all0(self):
+        E0 = [[0,1],[1,2],[2,1,0]]
+        w0 = [1.0,2.0,0.5]
+        E1 = []
+        w1 = []
+
+        x = {0:0,1:0,2:0}
+        self.assertEqual(sa.submodEval(x,E0,E1,w0,w1),0.)
+
+        x = {0:1.,1:1.,2:1.}
+        self.assertEqual(sa.submodEval(x,E0,E1,w0,w1),0.)
+        
+        x = {0:0.9,1:0.3,2:0}
+        self.assertAlmostEqual(sa.submodEval(x,E0,E1,w0,w1),1.725,places)
+
+    def test_all1(self):
+        E1 = [[0,1],[1,2],[2,1,0]]
+        w1 = [1.0,2.0,0.5]
+        E0 = []
+        w0 = []
+
+        x = {0:0,1:0,2:0}
+        self.assertEqual(sa.submodEval(x,E0,E1,w0,w1),3.5)
+
+        x = {0:1.,1:1.,2:1.}
+        self.assertEqual(sa.submodEval(x,E0,E1,w0,w1),0.0)
+        
+        x = {0:0.9,1:0.3,2:0}
+        self.assertAlmostEqual(sa.submodEval(x,E0,E1,w0,w1),3.23,places)
+
+    def test_mix(self):
+        E0 = [[0,1],[1,2],[2,1,0]]
+        w0 = [1.0,2.0,0.5]
+        E1 = [[0,1],[1,2],[2,1,0]]
+        w1 = [1.0,2.0,0.5]
+        x = {0:0.9,1:0.3,2:0}
+        self.assertAlmostEqual(sa.submodEval(x,E0,E1,w0,w1),3.23+1.725,places)
+
+#--------------------- Alpha Exp --------------------------------#
+
+class TestAExpSubproblem(unittest.TestCase):
+    def test_empty_alpha(self):
+        VOrig = [1,2,3,4]
+        EOrig = [[1,2],[3,4],[2,3,4]]
+        wOrig = [1.0,2.0,3.0]
+        labels = {1:1,2:1,3:2,4:2}
+        alpha = 3
+
+        E0,w0,E1,w1 = sa.AExpSubproblem(VOrig,EOrig,wOrig,labels,alpha)
+        self.assertEqual(E0,[[1,2],[3,4]])
+        self.assertEqual(w0,[1.0,2.0])
+        self.assertEqual(E1,[[2,3,4]])
+        self.assertEqual(w1,[3.0])
+
+    def test_three_clusters(self):
+        VOrig = [1,2,3,4,5,6]
+        EOrig = [[1,2],[3,4],[2,3,4],[5,6],[5,1,4],[5,1,2]]
+        wOrig = [1.0,2.0,3.0,4.0,5.0,6.0]
+        labels = {1:1,2:1,3:2,4:2,5:3,6:3}
+        alpha = 3
+
+        E0,w0,E1,w1 = sa.AExpSubproblem(VOrig,EOrig,wOrig,labels,alpha)
+        self.assertEqual(E0,[[1,2],[3,4]])
+        self.assertEqual(w0,[1.0,2.0])
+        self.assertEqual(E1,[[2,3,4],[5,1,4],[5,1,2]])
+        self.assertEqual(w1,[3.0,5.0,6.0])
+
+class TestAExpUpdate(unittest.TestCase):
+    def test_simple(self):
+        labels = {0:1,1:1,2:2,3:2,4:3}
+        alpha = 3
+
+        x = {0:0,1:0,2:0,3:0,4:0}
+        newlabels = {0:1,1:1,2:2,3:2,4:3}
+        self.assertEqual(sa.AExpUpdate(labels,x,alpha),newlabels)
+
+        x[3] = 1.
+        x[0] = 1.
+        newlabels = {0:3,1:1,2:2,3:3,4:3}
+        self.assertEqual(sa.AExpUpdate(labels,x,alpha),newlabels)
+
+#--------------------- AB Swaps --------------------------------#
+
+class TestABSubproblem(unittest.TestCase):
+    def test_empty_alpha(self):
+        VOrig = [1,2,3,4]
+        EOrig = [[1,2],[3,4],[2,3,4]]
+        wOrig = [1.0,2.0,3.0]
+        labels = {1:1,2:1,3:2,4:2}
+        alpha = 3
+        beta = 1
+
+        V,E0,w0 = sa.ABSubproblem(VOrig,EOrig,wOrig,labels,alpha,beta)
+        self.assertEqual(V,[1,2])
+        self.assertEqual(E0,[[1,2]])
+        self.assertEqual(w0,[1.0])
+
+    def test_larger_example(self):
+        VOrig = [1,2,3,4,5,6]
+        EOrig = [[1,2],[3,4],[2,3,4],[5,6],[5,1,4],[5,1,2]]
+        wOrig = [1.0,2.0,3.0,4.0,5.0,6.0]
+        labels = {1:1,2:1,3:2,4:2,5:3,6:3}
+        alpha = 3
+        beta = 1
+
+        V,E0,w0 = sa.ABSubproblem(VOrig,EOrig,wOrig,labels,alpha,beta)
+        self.assertEqual(V,[1,2,5,6])
+        self.assertEqual(E0,[[1,2],[5,6],[5,1,2]])
+        self.assertEqual(w0,[1.0,4.0,6.0])
+
+class TestABUpdate(unittest.TestCase):
+    def test_simple(self):
+        labels = {1:1,2:1,3:2,4:2,5:3,6:3}
+        alpha = 3
+        beta = 1
+
+        x = {1:0,2:0,5:0,6:0}
+        newlabels = {1:1,2:1,3:2,4:2,5:1,6:1}
+        self.assertEqual(sa.ABUpdate(labels,x,alpha,beta),newlabels)
+
+        x[2] = 1.
+        x[6] = 1.
+        newlabels = {1:1,2:3,3:2,4:2,5:1,6:3}
+        self.assertEqual(sa.ABUpdate(labels,x,alpha,beta),newlabels)
+
+#--------------------- Submod Alg --------------------------------#
+
+class TestSubmodAlg(unittest.TestCase):
+    def test_AB_simple(self):
+        VOrig = [1,2,3,4,5,6]
+        EOrig = [[1,3],[1,4],[1,5],[1,6],[2,3],[2,4],[2,5],[2,6],[3,5],[3,6],
+                 [4,5],[4,6],[1,3,5],[2,4,6],[1,3,6]]
+        wOrig = [1.0 for _ in range(15)]
+        
+        labels = {1:1,2:1,3:1,4:1,5:2,6:2}
+        alpha = 3
+        beta = 1
+        moveType = 'ab'
+        newlabels = sa.submodAlg(VOrig,EOrig,wOrig,labels,moveType,alpha,beta)
+        self.assertEqual(newlabels[5],2)
+        self.assertEqual(newlabels[6],2)
+        self.assertEqual(newlabels[1],newlabels[2])
+        self.assertEqual(newlabels[3],newlabels[4])
+        self.assertIn(newlabels[1],[1,3])
+        self.assertIn(newlabels[3],[1,3])
+        self.assertNotEqual(newlabels[1],newlabels[3])
+
+        labels = {1:1,2:3,3:1,4:3,5:2,6:2}
+        alpha = 3
+        beta = 1
+        newlabels = sa.submodAlg(VOrig,EOrig,wOrig,labels,moveType,alpha,beta)
+        self.assertEqual(newlabels[5],2)
+        self.assertEqual(newlabels[6],2)
+        self.assertEqual(newlabels[1],newlabels[2])
+        self.assertEqual(newlabels[3],newlabels[4])
+        self.assertIn(newlabels[1],[1,3])
+        self.assertIn(newlabels[3],[1,3])
+        self.assertNotEqual(newlabels[1],newlabels[3])
+
+    def test_AExp_simple(self):
+        VOrig = [1,2,3,4,5,6]
+        EOrig = [[1,3],[1,4],[1,5],[1,6],[2,3],[2,4],[2,5],[2,6],[3,5],[3,6],
+                 [4,5],[4,6],[1,3,5],[2,4,6],[1,3,6]]
+        wOrig = [1.0 for _ in range(15)]
+
+        labels = {1:1,2:1,3:1,4:2,5:2,6:2}
+        alpha = 3
+        moveType = 'aexp'
+        newlabels = sa.submodAlg(VOrig,EOrig,wOrig,labels,moveType,alpha)
+        self.assertEqual(newlabels[5],2)
+        self.assertEqual(newlabels[6],2)
+        self.assertEqual(newlabels[1],1)
+        self.assertEqual(newlabels[2],1)
+        self.assertEqual(newlabels[3],3)
+        self.assertEqual(newlabels[4],3)
+
+        labels = {1:1,2:1,3:2,4:2,5:2,6:3}
+        alpha = 3
+        moveType = 'aexp'
+        newlabels = sa.submodAlg(VOrig,EOrig,wOrig,labels,moveType,alpha)
+        self.assertEqual(newlabels[5],3)
+        self.assertEqual(newlabels[6],3)
+        self.assertEqual(newlabels[1],1)
+        self.assertEqual(newlabels[2],1)
+        self.assertEqual(newlabels[3],2)
+        self.assertEqual(newlabels[4],2)
+
+#--------------------- Local Search --------------------------------#
+class TestLocalSearchSubmod(unittest.TestCase):
+    def test_AB_simple(self):
+        V = [1,2,3,4,5,6]
+        E = [[1,3],[1,4],[1,5],[1,6],[2,3],[2,4],[2,5],[2,6],[3,5],[3,6],
+                 [4,5],[4,6],[1,3,5],[2,4,6],[1,3,6]]
+        w = [1.0 for _ in range(15)]
+
+        labels = {1:1,2:1,3:1,4:1,5:1,6:1}
+        moveType = 'ab'
+        k = 3
+        newlabels = sa.localSearchSubmod(V,E,w,k,labels,moveType)
+        self.assertEqual(newlabels[1],newlabels[2])
+        self.assertEqual(newlabels[3],newlabels[4])
+        self.assertEqual(newlabels[5],newlabels[6])
+        self.assertNotEqual(newlabels[1],newlabels[3])
+        self.assertNotEqual(newlabels[1],newlabels[5])
+        self.assertNotEqual(newlabels[3],newlabels[5])
+
+        labels = {1:1,2:2,3:0,4:1,5:1,6:2}
+        newlabels = sa.localSearchSubmod(V,E,w,k,labels,moveType)
+        self.assertEqual(newlabels[1],newlabels[2])
+        self.assertEqual(newlabels[3],newlabels[4])
+        self.assertEqual(newlabels[5],newlabels[6])
+        self.assertNotEqual(newlabels[1],newlabels[3])
+        self.assertNotEqual(newlabels[1],newlabels[5])
+        self.assertNotEqual(newlabels[3],newlabels[5])
+
+    def test_AExp_simple(self):
+        V = [1,2,3,4,5,6]
+        E = [[1,3],[1,4],[1,5],[1,6],[2,3],[2,4],[2,5],[2,6],[3,5],[3,6],
+                 [4,5],[4,6],[1,3,5],[2,4,6],[1,3,6]]
+        w = [1.0 for _ in range(15)]
+
+        labels = {1:1,2:1,3:1,4:1,5:1,6:1}
+        moveType = 'aexp'
+        k = 3
+        newlabels = sa.localSearchSubmod(V,E,w,k,labels,moveType)
+        self.assertEqual(newlabels[1],newlabels[2])
+        self.assertEqual(newlabels[3],newlabels[4])
+        self.assertEqual(newlabels[5],newlabels[6])
+        self.assertNotEqual(newlabels[1],newlabels[3])
+        self.assertNotEqual(newlabels[1],newlabels[5])
+        self.assertNotEqual(newlabels[3],newlabels[5])
+
+class TestLocalSearch(unittest.TestCase):
+    def test_simple(self):
+        V = [1,2,3,4,5,6]
+        E = [[1,3],[1,4],[1,5],[1,6],[2,3],[2,4],[2,5],[2,6],[3,5],[3,6],
+                 [4,5],[4,6],[1,3,5],[2,4,6],[1,3,6]]
+        w = [1.0 for _ in range(15)]
+
+        labels = {1:1,2:1,3:1,4:1,5:1,6:1}
+        k = 3
+        newlabels = sa.localSearch(V,E,w,k,labels)
+        self.assertEqual(newlabels[1],newlabels[2])
+        self.assertEqual(newlabels[3],newlabels[4])
+        self.assertEqual(newlabels[5],newlabels[6])
+        self.assertNotEqual(newlabels[1],newlabels[3])
+        self.assertNotEqual(newlabels[1],newlabels[5])
+        self.assertNotEqual(newlabels[3],newlabels[5])
+        
+
+if __name__ == '__main__':
+    unittest.main()
